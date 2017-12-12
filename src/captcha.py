48d66b4ff9a805b10baaad24ae2dc4f3f1efc956
Diogo Rolo <diogo.rolo@outlook.com>
2017-12-12 00:08:58 +0000

    Implements solver

--- src/captcha.py
+++ src/captcha.py
@@ -1,9 +1,19 @@
 
 
 class Captcha:
-    def solve(self, digits):
-        pass
+    @staticmethod
+    def solve(digits):
+        if len(digits) == 0:
+            return 0
+        total = 0
+        for i in range(len(digits)-1):
+            if digits[i] == digits[i+1]:
+                total += int(digits[i])
+        if digits[0] == digits[-1]:
+            total += int(digits[0])
+        return total
 
 
 if __name__ == '__main__':
-    print("Hello AdventOfCode!")
+    puzzle_input = ''
+    print(Captcha.solve(puzzle_input))
