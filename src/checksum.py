e860d2df977584624f02c0bf29181bf9c7f2dedf
Diogo Rolo <diogo.rolo@outlook.com>
2017-12-14 21:17:26 +0000

    Adds day two barebones

---
+++ src/checksum.py
@@ -0,0 +1,17 @@
+
+
+class Checksum:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def solve(self):
+        pass
+
+    def line_difference(self, line_idx):
+        pass
+
+
+if __name__ == '__main__':
+    puzzle_input = ''
+    checksum_solver = Checksum(puzzle_input)
+    print(checksum_solver.solve())
---
+++ src/test_checksum.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from checksum import Checksum
+
+
+class TestChecksum(TestCase):
+    def setUp(self):
+        c = Checksum()
+
+    def test_differenceInRow(self):
+        self.fail()
+
+    def test_totalChecksum(self):
+        self.fail()
+
+    def test_emptyInput(self):
+        self.fail()
