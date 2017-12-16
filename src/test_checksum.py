64bf6ed9a272e641499aa5329d8122875617e6cf
Diogo Rolo <diogo.rolo@outlook.com>
2017-12-16 19:29:03 +0000

    Completes tests for day 2

--- src/test_checksum.py
+++ src/test_checksum.py
@@ -3,14 +3,37 @@ from checksum import Checksum
 
 
 class TestChecksum(TestCase):
-    def setUp(self):
-        c = Checksum()
-
     def test_differenceInRow(self):
-        self.fail()
+        input_data = \
+            '5 1 9 5\
+            7 5 3\
+            2 4 6 8'
+        c = Checksum(input_data)
+        self.assertEqual(18, c.line_difference(0))
+        self.assertEqual(4, c.line_difference(1))
+        self.assertEqual(6, c.line_difference(2))
 
     def test_totalChecksum(self):
-        self.fail()
+        input_data = \
+            '5 1 9 5\
+            7 5 3\
+            2 4 6 8'
+        c = Checksum(input_data)
+        self.assertEqual(18, c.solve())
+
+    def test_parseInput(self):
+        input_data = \
+            '5 1 9 5\
+            7 5 3\
+            2 4 6 8'
+        c = Checksum(input_data)
+        expected_data = [[5, 1, 9, 5],
+                         [7, 5, 3],
+                         [2, 4, 6, 8]]
+
+        self.assertEqual(expected_data, c.matrix)
 
     def test_emptyInput(self):
-        self.fail()
+        input_data = ''
+        c = Checksum(input_data)
+        self.assertEqual(0, c.solve())
