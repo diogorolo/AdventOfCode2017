f9381487afe0e804f509f263a0329d0d4f7551e7
Diogo Rolo <diogo.rolo@outlook.com>
2017-12-17 19:55:41 +0000

    All passing tests

--- src/checksum.py
+++ src/checksum.py
@@ -1,14 +1,24 @@
 
 
 class Checksum:
-    def __init__(self, matrix):
-        self.matrix = matrix
+    def __init__(self, matrix_text):
+        self.matrix = self.__parse(self.__sanitize(matrix_text))
+
+    def __sanitize(self,matrix_text):
+        return matrix_text.replace('\t', ' ')
+
+    def __parse(self, matrix_text):
+        return [list(map(int, x.split(' '))) for x in matrix_text.split('\r\n') if x != '']
 
     def solve(self):
-        pass
+        result = 0
+        for i in range(len(self.matrix)):
+            result += self.line_difference(i)
+        return result
 
     def line_difference(self, line_idx):
-        pass
+        line = self.matrix[line_idx]
+        return max(line) - min(line)
 
 
 if __name__ == '__main__':
--- src/test_checksum.py
+++ src/test_checksum.py
@@ -4,28 +4,26 @@ from checksum import Checksum
 
 class TestChecksum(TestCase):
     def test_differenceInRow(self):
-        input_data = \
-            '5 1 9 5\
-            7 5 3\
-            2 4 6 8'
+        input_data = ("5 1 9 5\r\n"
+                      "7 5 3\r\n"
+                      "2 4 6 8")
         c = Checksum(input_data)
-        self.assertEqual(18, c.line_difference(0))
+        self.assertEqual(8, c.line_difference(0))
         self.assertEqual(4, c.line_difference(1))
         self.assertEqual(6, c.line_difference(2))
 
     def test_totalChecksum(self):
-        input_data = \
-            '5 1 9 5\
-            7 5 3\
-            2 4 6 8'
+        input_data = ("5 1 9 5\r\n"
+                      "7 5 3\r\n"
+                      "2 4 6 8")
         c = Checksum(input_data)
         self.assertEqual(18, c.solve())
 
     def test_parseInput(self):
-        input_data = \
-            '5 1 9 5\
-            7 5 3\
-            2 4 6 8'
+        input_data = ("5 1 9 5\r\n"
+                      "7 5 3\r\n"
+                      "2 4 6 8")
+
         c = Checksum(input_data)
         expected_data = [[5, 1, 9, 5],
                          [7, 5, 3],
