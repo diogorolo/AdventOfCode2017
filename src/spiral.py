59657bd288163acf3ce03757380470e55fbb7ba2
Diogo Rolo <diogo.rolo@outlook.com>
2017-12-20 18:52:12 +0000

    Solves spiral with more tests

--- src/spiral.py
+++ src/spiral.py
@@ -1,14 +1,34 @@
+from math import ceil, sqrt, copysign, floor
 
 
 class Spiral:
     def __init__(self, number):
         self.number = number
 
+    def matrix_size(self):
+        result = ceil(sqrt(self.number))
+        if result % 2 == 0:
+            result += 1
+        return result
+
+    def side_distance(self):
+        size = self.matrix_size()
+        side_number = size*size
+
+        for i in range(3):
+            if side_number <= self.number:
+                break
+            side_number -= size - 1
+        center_number = side_number + copysign(floor(size/2), self.number - side_number)
+        distance = abs(self.number - center_number)
+        return distance
+
     def solve(self):
-        pass
+        distance = self.side_distance() + floor(self.matrix_size() / 2)
+        return distance
 
 
 if __name__ == '__main__':
-    puzzle_input = 1
+    puzzle_input = 368078
     spiral = Spiral(puzzle_input)
     print(spiral.solve())
--- src/test_spiral.py
+++ src/test_spiral.py
@@ -22,3 +22,33 @@ class TestSpiral(TestCase):
         s = Spiral(1024)
         result = s.solve()
         self.assertEqual(31, result)
+
+    def test_matrix_size_same_spot(self):
+        s = Spiral(1)
+        result = s.matrix_size()
+        self.assertEqual(1, result)
+
+    def test_matrix_size_one_away(self):
+        s = Spiral(8)
+        result = s.matrix_size()
+        self.assertEqual(3, result)
+
+    def test_matrix_size_four_away(self):
+        s = Spiral(26)
+        result = s.matrix_size()
+        self.assertEqual(7, result)
+
+    def test_side_distance_centered(self):
+        s = Spiral(11)
+        result = s.side_distance()
+        self.assertEqual(0, result)
+
+    def test_side_distance_one_offset(self):
+        s = Spiral(3)
+        result = s.side_distance()
+        self.assertEqual(1, result)
+
+    def test_side_distance_corner(self):
+        s = Spiral(25)
+        result = s.side_distance()
+        self.assertEqual(2, result)
