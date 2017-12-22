8c1a8b75b9b023897ef8097a23c0ca4df9d7a84b
Diogo Rolo <diogo.rolo@outlook.com>
2017-12-22 16:25:12 +0000

    Solves reallocation

--- src/reallocation.py
+++ src/reallocation.py
@@ -12,17 +12,29 @@ class Reallocation:
                 return idx
 
     def distribute(self):
-        pass
+        start_idx = self.largest_memory_idx()
+        memory_amount = self.current_state[start_idx]
+        self.current_state[start_idx] = 0
+        start_idx += 1
+        for i in range(memory_amount):
+            self.current_state[(start_idx + i) % len(self.current_state)] += 1
+        if self.is_repeated_state():
+            return False
+
+        self.save_state()
+        return True
 
     def save_state(self):
-        self.memory.append(self.current_state)
+        self.memory.append(list(self.current_state))
 
     def is_repeated_state(self):
         return self.current_state in self.memory
 
     def solve(self):
-        pass
-
+        iterations = 1
+        while self.distribute():
+            iterations += 1
+        return iterations
 
 if __name__ == '__main__':
     input_data = [int(x) for x in '0 2 7 0'.split(' ')]
